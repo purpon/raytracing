52c3fa910a9f22c51e3b4a5c2763c90e3ccd27d9
John Cheng <purpon@hotmail.com>
2017-03-01 14:32:44 +0800

    Change to posix thread compatible function prototype

--- raytracing.h
+++ raytracing.h
@@ -4,8 +4,5 @@
 #include "objects.h"
 #include <stdint.h>
 
-void raytracing(uint8_t *pixels, color background_color,
-                rectangular_node rectangulars, sphere_node spheres,
-                light_node lights, const viewpoint *view,
-                int width, int height);
+void *raytracing(void *);
 #endif
